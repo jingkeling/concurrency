e0d066568faaae6dde186874370ac060113a8b08
jingkeling <841127052@qq.com>
2018-05-26 16:32:50 +0800

    ~

--- src/main/java/com/kolin/Controller/TestController.java
+++ src/main/java/com/kolin/Controller/TestController.java
@@ -13,8 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 
+
+
+    /*
+    [nio-8080-exec-1]
+[nio-8080-exec-4]
+[nio-8080-exec-2]
+[nio-8080-exec-5]
+[nio-8080-exec-3]
+[nio-8080-exec-6]
+[nio-8080-exec-7]
+[io-8080-exec-12]
+[io-8080-exec-10]
+[io-8080-exec-11]
+[nio-8080-exec-4]
+[io-8080-exec-19]
+[nio-8080-exec-4]
+     */
+    /**
+     *
+     * @return
+     */
     @RequestMapping("/test")
     public String test() {
+        log.info("1");
         return "test";
     }
 
